8e82fa51f8336222cfdc28f0ba33e35d630b95dc
Gleb Petrovicev <gpetrovichev@purestorage.com>
2023-12-04 19:05:01 +0100

    Day 4 star 2

--- 4/main.py
+++ 4/main.py
@@ -1,14 +1,20 @@
 import fileinput
 
+scratchcards = 0
 with fileinput.input(files=('input'), encoding="utf-8") as f:
-    points_sum = 0
+    next_multiplier = []
     for line in f:
+        multiplier = len(next_multiplier) + 1
+        next_multiplier = [i - 1 for i in next_multiplier if i > 1]
+
         card, numbers = line.split(": ")
         winning_numbers, actual_numbers = numbers.split(" | ")
         winning_numbers = [int(i) for i in winning_numbers.strip(" \n").split()]
         actual_numbers = [int(i) for i in actual_numbers.strip(" \n").split()]
-        power = sum(1 for i in actual_numbers if i in winning_numbers) - 1
-        points = 0 if power == -1 else pow(2, power)
-        print("{}: {} point(s)".format(card, points))
-        points_sum += points
-    print("Points sum: {}".format(points_sum))
+        matches = sum(1 for i in actual_numbers if i in winning_numbers)
+        if matches != 0:
+            for i in range(multiplier):
+                next_multiplier.append(matches)
+        print("{}: {} match(es) x {}, gives {} multiplier".format(card, matches, multiplier, next_multiplier))
+        scratchcards += multiplier
+print("{} scratchcards".format(scratchcards))
