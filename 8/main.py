513e34121317fb9633cc7fddbf0198dc24b853a6
Gleb Petrovicev <gpetrovichev@purestorage.com>
2023-12-08 14:40:54 +0100

    Day 8 star 2

--- 8/main.py
+++ 8/main.py
@@ -1,24 +1,42 @@
 import fileinput
-from typing import Dict, Any
+from math import gcd
+from functools import reduce
 
 instructions = ""
 m: dict[str, tuple[str, str]] = {}
+current_keys = []
 with fileinput.input(files=('input'), encoding="utf-8") as f:
     for i, line in enumerate(f):
         if i == 0:
             instructions = line.strip()
         if i > 1:
+            key: str
             key, turns = line.split(" = ")
             left, right = turns.strip("()\n").split(', ')
             m[key] = (left, right)
+            if key.endswith("A"):
+                current_keys.append(key)
 
-current_key = "AAA"
 i = 0
-while current_key != "ZZZ":
-    # print(current_key, m[current_key])
-    # print(instructions[i % len(instructions)], [0 if instructions[i % len(instructions)] == "L" else 1])
-    current_key = m[current_key][0 if instructions[i % len(instructions)] == "L" else 1]
-    # print(current_key)
+
+z_count = 0
+print(current_keys)
+previous_z = [0 for _ in current_keys]
+z_freq = [0 for _ in current_keys]
+freq_stab = [0 for _ in current_keys]
+
+while len([x for x in freq_stab if x < 1]) > 0:
+    for j in range(len(current_keys)):
+        current_keys[j] = m[current_keys[j]][0 if instructions[i % len(instructions)] == "L" else 1]
+        if current_keys[j].endswith("Z"):
+            if z_freq[j] == i - previous_z[j]:
+                freq_stab[j] += 1
+            else:
+                freq_stab[j] -= 1
+            z_freq[j] = i - previous_z[j]
+            print(z_freq)
+            previous_z[j] = i
     i += 1
 
-print(i)
+lcm = reduce(lambda x, y: x * y // gcd(x, y), z_freq)
+print(lcm)
