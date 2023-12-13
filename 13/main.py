001e3d97c948fbda4ac2a56e9d7ecc24b91aa78b
Gleb Petrovicev <gpetrovichev@purestorage.com>
2023-12-13 14:41:41 +0100

    Day 13 star 2

--- 13/main.py
+++ 13/main.py
@@ -9,34 +9,40 @@ def out_group(g: list[list[str]]):
 
 
 def find_mirror(g: list[list[str]]) -> int:
-    common_mirror_points: list[int] = []
+    common_mirror_points: dict[int, int] = {}
 
     for i, _ in enumerate(g):
-        line_mirror_points: list[int] = []
+        line_mirror_points: dict[int, int] = {}
         for j, _ in enumerate(g[i]):
             k = 0
             if j != 0:
-                mirror_point = True
+                smudges = 0
                 while j + k < len(g[i]) and j - k - 1 >= 0:
                     if g[i][j + k] != g[i][j - k - 1]:
-                        mirror_point = False
-                        break
+                        smudges += 1
+                        if smudges > 1:
+                            break
                     k += 1
-                if mirror_point:
-                    line_mirror_points.append(j)
+
+                if 0 <= smudges <= 1:
+                    line_mirror_points[j] = smudges
 
         if i == 0:
             if len(line_mirror_points) == 0:
                 return 0
             common_mirror_points = line_mirror_points
         else:
-            common_mirror_points = [p for p in common_mirror_points if p in line_mirror_points]
+            # common_mirror_points = [p for p in common_mirror_points if p in line_mirror_points]
+            common_mirror_points = {pos: sm + line_mirror_points[pos] for pos, sm in common_mirror_points.items() if pos in line_mirror_points and sm + line_mirror_points[pos] < 2}
             if len(common_mirror_points) == 0:
                 return 0
 
-    assert len(common_mirror_points) <= 1
+    # assert len(common_mirror_points) <= 1
 
-    return common_mirror_points[0] if len(common_mirror_points) == 1 else 0
+    common_mirror_points = {pos: sm for pos, sm in common_mirror_points.items() if sm == 1}
+    assert 0 <= len(common_mirror_points) <= 1
+
+    return list(common_mirror_points.keys())[0] if len(common_mirror_points) == 1 else 0
 
 
 def transpose(original: list[list[str]]) -> list[list[str]]:
@@ -52,7 +58,6 @@ with fileinput.input(files=("input"), encoding="utf-8") as f:
     group = []
     for line in f:
         if line == "\n":
-            # out_group(group)
             hor = find_mirror(group)
             if hor != 0:
                 sum += hor
@@ -60,6 +65,7 @@ with fileinput.input(files=("input"), encoding="utf-8") as f:
                 ver = find_mirror(transpose(group))
                 if ver != 0:
                     sum += ver * 100
+            # out_group(group)
             # print(find_mirror(group))
             # out_group(transpose(group))
             # print(find_mirror(transpose(group)))
