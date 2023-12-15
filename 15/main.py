df01bb64de2b6c50780b2672a8e53a9770ccdf94
Gleb Petrovicev <gpetrovichev@purestorage.com>
2023-12-15 07:26:03 +0100

    Day 15 star 2

--- 15/main.py
+++ 15/main.py
@@ -1,19 +1,57 @@
 import fileinput
 
 sum_hash = 0
+boxes: list[list[tuple[str, int]]] = [[] for i in range(256)]
+
+
+def print_boxes():
+    for i, box in enumerate(boxes):
+        if len(box) == 0:
+            continue
+        print("Box {}: ".format(i), end="")
+        for lens in box:
+            print("[{} {}] ".format(lens[0], lens[1]), end="")
+        print()
+    print()
+
 
 with (fileinput.input(files=("input"), encoding="utf-8") as f):
     res_hash = 0
+    label = ""
     for line in f:
         for c in line:
+            if c == '=':
+                continue
+            if '0' <= c <= '9':
+                updated = False
+                for i, _ in enumerate(boxes[res_hash]):
+                    if boxes[res_hash][i][0] == label:
+                        boxes[res_hash][i] = (label, int(c))
+                        updated = True
+                        break
+                if not updated:
+                    boxes[res_hash].append((label, int(c)))
+
+                # print("After \"{}={}\":".format(label, c))
+                # print_boxes()
+            if c == '-':
+                for i, _ in enumerate(boxes[res_hash]):
+                    if boxes[res_hash][i][0] == label:
+                        boxes[res_hash].pop(i)
+                        break
+
+                # print("After \"{}-\":".format(label))
+                # print_boxes()
             if c == ',' or c == '\n':
-                # print("- ", res_hash)
-                sum_hash += res_hash
                 res_hash = 0
+                label = ""
                 continue
 
-            # print(c, end=" ")
+            label += c
             res_hash = (res_hash + ord(c)) * 17 % 256
-            # print(res_hash)
 
-print(sum_hash)
+focal_sum = 0
+for i, lenses in enumerate(boxes):
+    for j, lens in enumerate(lenses):
+        focal_sum += (i + 1) * (j + 1) * lens[1]
+print(focal_sum)
