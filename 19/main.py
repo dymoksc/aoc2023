df1da46dfba35f9e3537d22b14738c6bf6fa5d30
Gleb Petrovicev <gpetrovichev@purestorage.com>
2023-12-19 20:19:59 +0100

    Day 19 star 2

--- 19/main.py
+++ 19/main.py
@@ -44,11 +44,11 @@ def consider(p: dict[str, tuple[int, int]], flow: str = "in", c_ind: int = 0, tr
             if cond.result == "R":
                 pass
             elif cond.result == "A":
-                mod_p[cond.attr] = (cond.num + 1, p[cond.attr][1])
+                mod_p[cond.attr] = (max(p[cond.attr][0], cond.num + 1), p[cond.attr][1])
                 accepted.append(mod_p)
                 print(trace + [flow], mod_p)
             else:
-                mod_p[cond.attr] = (cond.num + 1, p[cond.attr][1])
+                mod_p[cond.attr] = (max(p[cond.attr][0], cond.num + 1), p[cond.attr][1])
                 consider(mod_p, cond.result, 0, trace + [flow])
         if p[cond.attr][0] <= cond.num:  # False branch
             mod_p_alt = copy.deepcopy(p)
@@ -68,12 +68,38 @@ def consider(p: dict[str, tuple[int, int]], flow: str = "in", c_ind: int = 0, tr
         if p[cond.attr][1] > cond.num - 1:  # False branch
             mod_p_alt = copy.deepcopy(p)
             mod_p_alt[cond.attr] = (cond.num, p[cond.attr][1])
-            consider(p, flow, c_ind + 1, trace + [flow])
+            consider(mod_p_alt, flow, c_ind + 1, trace + [flow])
     else:
         consider(p, flow, c_ind + 1, trace + [flow])
 
 
-with (fileinput.input(files=("input_sample"), encoding="utf-8") as f):
+def consider1(p) -> (bool, list[str]):
+    global auto
+
+    trace = []
+    flow = "in"
+    while True:
+        trace.append(flow)
+        automaton_conds = auto[flow]
+        finish_current_automaton = False
+        while not finish_current_automaton:
+            for cond in automaton_conds:
+                res = None
+                if cond.sign == '>' and p[cond.attr] > cond.num or cond.sign == '<' and p[cond.attr] < cond.num:
+                    res = cond.result
+                if res is None:
+                    continue
+                elif res == 'A':
+                    return True, trace
+                elif res == 'R':
+                    return False, trace
+                else:
+                    finish_current_automaton = True
+                    flow = res
+                    break
+
+
+with (fileinput.input(files=("input"), encoding="utf-8") as f):
     for line in f:
         m = re.match('(\w+)\{(.*)}\n', line)
         if m is None:
@@ -89,6 +115,25 @@ with (fileinput.input(files=("input_sample"), encoding="utf-8") as f):
 
     for line in f:
         parts.append({k: int(v) for k, v in [assign.split('=') for assign in line.strip('{}\n').split(',')]})
+
+# # ['in', 'px', 'qkq'] {'x': (1, 1416), 'm': (1, 4001), 'a': (1, 2006), 's': (1, 1351)}
+# for acc in accepted:
+#     for x in [1, 1415]:
+#         for m in [1, 4000]:
+#             for a in [1, 2005]:
+#                 for s in [1, 1350]:
+#                     res, trace = consider1({"x": x, "m": m, "a": a, "s": s})
+#                     assert (res, trace) == (True, ['in', 'px', 'qkq']), trace
+#
+# # ['in', 'in', 'qqz', 'qqz', 'hdj', 'hdj', 'pv', 'pv'] {'x': (1, 4001), 'm': (1, 839), 'a': (1, 1717), 's': (1, 2771)}
+# for x in [1, 4000]:
+#     for m in [1, 838]:
+#         for a in [1, 1716]:
+#             for s in [1, 2770]:
+#                 res, trace = consider1({"x": x, "m": m, "a": a, "s": s})
+#                 print(trace)
+#                 assert res
+#                 # assert (res, trace) == (True, ['in', 'qqz', 'hdj', 'pv']), trace
 
 
 def get_volume(p: dict[str, tuple[int, int]]) -> int:
@@ -122,6 +167,18 @@ assert get_volume({"x": (0, 2), "y": (0, 2)}) - \
 
 
 consider({"x": (1, 4001), "m": (1, 4001), "a": (1, 4001), "s": (1, 4001)})
+
+for acc in accepted:
+    for x in [acc["x"][0], acc["x"][1] - 1]:
+        for m in [acc["m"][0], acc["m"][1] - 1]:
+            for a in [acc["a"][0], acc["a"][1] - 1]:
+                for s in [acc["s"][0], acc["s"][1] - 1]:
+                    res, _ = consider1({"x": x, "m": m, "a": a, "s": s})
+                    if not res:
+                        # ['in', 'mb', 'hvq', 'hsn', 'drn', 'drn', 'drn', 'jff', 'jff', 'jn', 'jn', 'jn'] {'x': (1, 4001), 'm': (611, 1446), 'a': (3252, 3412), 's': (1, 804)}
+                        pass
+                    assert res
+                    # assert (res, trace) == (True, ['in', 'px', 'qkq']), trace
 
 # 167409079868000
 # 183726960685100
